72b3b123ae7b978022bf94e53a916a38864afb71
Ricardo Marliere <rbmarliere@gmail.com>
2019-01-15 02:38:18 +0000

    new table structures

--- stakemine.token/include/stakemine.token/stakemine.token.hpp
+++ stakemine.token/include/stakemine.token/stakemine.token.hpp
@@ -97,17 +97,35 @@ namespace stakemine
             };
             typedef eosio::multi_index< "stat"_n, currency_stats > stats;
 
-            struct [[eosio::table]] staking_account
+            struct [[eosio::table]] bandwidth
             {
                 name           holder;
-                time_point_sec request_time;
+                name           contract;
                 asset          cpu_weight;
                 asset          net_weight;
+                time_point_sec request_time;
 
                 uint64_t primary_key()const { return holder.value; }
             };
-            typedef eosio::multi_index< "stake"_n, staking_account > holders;
+            typedef eosio::multi_index< "holders"_n, bandwidth > holders;
 
+            struct [[eosio::table]] listing
+            {
+                name     contract;
+                string   description;
+                string   url;
+                string   image_url;
+                uint64_t period;
+                asset    cpu_target;
+                asset    cpu_reward;
+                asset    cpu_total;
+                asset    net_target;
+                asset    net_reward;
+                asset    net_total;
+
+                uint64_t primary_key()const { return contract.value; }
+            };
+            typedef eosio::multi_index< "listing"_n, listing > listings;
     };
 }
 
