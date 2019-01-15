2bdd1e38e791ca69dab8cda57c664a5bbcd9ecfd
Ricardo Marliere <rbmarliere@gmail.com>
2019-01-15 02:33:11 +0000

    remove hard parameters from stakemine.deleg.hpp

--- stakemine.token/include/stakemine.token/stakemine.deleg.hpp
+++ stakemine.token/include/stakemine.token/stakemine.deleg.hpp
@@ -1,13 +1,6 @@
 #include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
 
-#define SYMBOL_CODE       "STK"
-#define SYMBOL_PRECISION  4
-#define STAKE_FACTOR      100  // EOS
-#define PERIOD_IN_SEC     3600 // 1h
-#define PERIOD_CPU_REWARD 1    // 1 token per STAKE_FACTOR
-#define PERIOD_NET_REWARD 1    // 1 token per STAKE_FACTOR
-
 namespace eosiosystem
 {
     using namespace eosio;
